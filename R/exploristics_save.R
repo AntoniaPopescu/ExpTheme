52c1cb5dee73d6c3a995fd4667378c58b20f8d4a
Antonia Popescu <93182796+AntoniaPopescu@users.noreply.github.com>
2022-01-24 09:06:52 +0000

    Add files via upload

---
+++ R/exploristics_save.R
@@ -0,0 +1,37 @@
+#' Save your Exploristics
+#'
+#' This function allows you to save your ggplot in predefined sizes
+#' This function takes as arguments: a filename location where to 
+#' save the plot to, the plot wanting to be saves, and a plotSize argument
+#' that can be either portrait, square,or landscape
+#' 
+#' @keywords exploristics_save
+#' @export
+
+
+#This function allows you to save your ggplot in predefined sizes
+exploristics_save = function(filename, myPlot, plotSize, ...){
+  
+  if (plotSize == "portrait") {
+    width = 640
+    height = 800
+  }
+  if (plotSize == "square") {
+    width = 640
+    height = 640
+  }
+  
+  if (plotSize == "landscape") {
+    width = 800
+    height = 640
+  }
+  
+  
+  ggsave(filename = filename,
+         plot = plotGrid,
+         device = "png",
+         width = width,
+         height = height,
+         units = "px",
+         ...)
+}
